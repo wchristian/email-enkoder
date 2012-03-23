d622f20f3bd145c8311f90dc9d2fa00ffd34800a
Christian Walde <walde.christian@googlemail.com>
2012-03-23 01:35:52 +0100

    initial implementation

--- lib/Email/Enkoder.pm
+++ lib/Email/Enkoder.pm
@@ -2,10 +2,150 @@ use strictures;
 
 package Email::Enkoder;
 
+use 5.010;
+use Sub::Exporter::Simple qw' enkode enkode_mail ';
+use utf8;
+
 # VERSION
 
-# ABSTRACT:
+# ABSTRACT: obfuscate email or html with randomized javascript
 
 # COPYRIGHT
+
+my $default_enkoders = [
+    {
+        perl => sub { reverse @_ },
+        js   => ";kode=kode.split('').reverse().join('')",
+    },
+    {
+        perl => sub {
+            my ( $string ) = @_;
+            for my $i ( 0 .. ( length( $string ) / 2 - 1 ) ) {
+                $i *= 2;
+                my $left = substr $string, $i, 1;
+                my $right = substr $string, $i + 1, 1;
+                substr $string, $i, 1, $right;
+                substr $string, $i + 1, 1, $left;
+            }
+            return $string;
+        },
+        js => (
+                ";x='';for(i=0;i<(kode.length-1);i+=2){"
+              . "x+=kode.charAt(i+1)+kode.charAt(i)"
+              . "}kode=x+(i<kode.length?kode.charAt(kode.length-1):'');"
+        )
+    },
+    {
+        perl => sub {
+            my ( $str ) = @_;
+            my @chars = split //, $str;
+            for ( @chars ) {
+                my $ord = ord $_;
+                $ord += 3;
+                $_ = chr $ord;
+            }
+            my $result = join '', @chars;
+            return $result;
+        },
+        js => (
+            ";x='';for(i=0;i<kode.length;i++){"    #
+              . "c=kode.charCodeAt(i)-3;x+=String.fromCharCode(c)"
+              . "}kode=x"
+        )
+    },
+];
+
+sub enkoders { $default_enkoders }
+
+sub enkode_mail {
+    my ( $email, $link_text, $options ) = @_;
+
+    $_ = defined $_ ? qq| $_|         : '' for $options->{link_attributes};
+    $_ = defined $_ ? qq|?subject=$_| : '' for $options->{subject};
+
+    $email = qq|<a href="mailto:$email$options->{subject}"$options->{link_attributes}>$link_text</a>|;
+    my $link = enkode( $email, $options );
+
+    return $link;
+}
+
+sub enkode {
+    my ( $html, $options ) = @_;
+
+    $options->{enkoders}   ||= enkoders();
+    $options->{max_length} ||= 1024;
+
+    my $js = js_dbl_quote( $html );
+    $js = qq|document.write($js);|;
+
+    $options->{max_length} = 1 + length $js if $options->{max_length} <= length $js;
+
+    my $script_html;
+
+    while ( $options->{max_length} > length $js ) {
+        my $idx = $options->{enkoder_index} // int rand @{ $options->{enkoders} };
+        $js = $options->{enkoders}[$idx]{perl}->( $js );
+        $js = js_dbl_quote( $js );
+        $js = qq|kode=$js$options->{enkoders}[$idx]{js}|;
+        my $js_variable = js_wrap_quote( js_dbl_quote( $js ), 79 );
+
+        $script_html = qq|
+            <script type="text/javascript">
+            /* <![CDATA[ */
+            function perl_enkoder(){var kode=
+            $js_variable
+            ;var i,c,x;while(eval(kode));}perl_enkoder();
+            /* ]]> */
+            </script>
+        |;
+        $script_html =~ s/^ +//mg;
+        last if length $script_html > $options->{max_length};
+    }
+
+    return $script_html;
+}
+
+sub js_wrap_quote {
+    my ( $str, $max_line_length ) = @_;
+
+    $max_line_length -= 3;
+    my $inQ;
+    my $esc     = 0;
+    my $lineLen = 0;
+    my $result  = '';
+    my $chunk   = '';
+
+    while ( length $str ) {
+        my $chunk = '';
+        if ( $str =~ /^\\[0-7]{3}/ ) {
+            $chunk = substr $str, 0, 4;
+            substr $str, 0, 4, '';
+        }
+        elsif ( $str =~ /^\\./ ) {
+            $chunk = substr $str, 0, 2;
+            substr $str, 0, 2, '';
+        }
+        else {
+            $chunk = substr $str, 0, 1;
+            substr $str, 0, 1, '';
+        }
+
+        if ( ( $lineLen + length $chunk ) >= $max_line_length ) {
+            $result .= qq|"+\n"|;
+            $lineLen = 1;
+        }
+
+        $lineLen += length $chunk;
+        $result .= $chunk;
+    }
+
+    return $result;
+}
+
+sub js_dbl_quote {
+    my ( $in ) = @_;
+    $in =~ s/([\\"])/\\$1/g;
+    return qq|"$in"|;
+}
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -4,14 +4,141 @@ package basic_test;
 
 use Test::InDistDir;
 use Test::More;
+use utf8;
+use open ':encoding(UTF-8)';
 
-use Email::Enkoder;
+use Email::Enkoder qw' enkode enkode_mail ';
 
 run();
 done_testing;
 exit;
 
 sub run {
+    my $input = "meüep";
+
+    my %enkoder_tests = enkoder_tests();
+    is( scalar @{ Email::Enkoder->enkoders }, scalar keys %enkoder_tests, "all enkoders have tests" );
+
+    test_encoder( $input, $_, $enkoder_tests{$_} ) for sort keys %enkoder_tests;
+
+    ok( test_randomness( $input ), "generation of random enkoder combinations works" );
+
+    is( Email::Enkoder::js_wrap_quote( "aaaaa\\123", 12 ),
+        qq|aaaaa"+\n"\\123|, "js_wrap_quote doesn't split up character sequences" );
+
+    test_mail_enkoding( $input );
 
     return;
+}
+
+sub test_mail_enkoding {
+    my ( $input ) = @_;
+
+    my $enkoders = [ { perl => sub { $_[0] }, js => "" } ];
+    my $mail = enkode_mail( $input, qq|"$input"|, { max_length => 1, enkoders => $enkoders } );
+
+    is(
+        $mail,
+        q|
+<script type="text/javascript">
+/* <![CDATA[ */
+function perl_enkoder(){var kode=
+"kode=\"document.write(\\\\\\"<a href=\\\\\\\\\\\\\\"mailto:meüep\\\\\\\\\\\\\\">\\\\\\\\\\\\\\"meü"+
+"ep\\\\\\\\\\\\\\"</a>\\\\\\");\""
+;var i,c,x;while(eval(kode));}perl_enkoder();
+/* ]]> */
+</script>
+|,
+        "email link generation works, as well as modification of enkoders list"
+    );
+
+    my $mail2 = enkode_mail(
+        $input,    #
+        qq|"$input"|,
+        {
+            max_length      => 1,
+            enkoders        => $enkoders,
+            link_attributes => qq|title="$input" class="$input"|,
+            subject         => $input
+        }
+    );
+
+    is(
+        $mail2,
+        q|
+<script type="text/javascript">
+/* <![CDATA[ */
+function perl_enkoder(){var kode=
+"kode=\"document.write(\\\\\\"<a href=\\\\\\\\\\\\\\"mailto:meüep?subject=meüep\\\\\\\\\\\\"+
+"\" title=\\\\\\\\\\\\\\"meüep\\\\\\\\\\\\\\" class=\\\\\\\\\\\\\\"meüep\\\\\\\\\\\\\\">\\\\\\\\\\\\\\"meüep\\\\"+
+"\\\\\\\\\\"</a>\\\\\\");\""
+;var i,c,x;while(eval(kode));}perl_enkoder();
+/* ]]> */
+</script>
+|,
+        "subject and link attribute options work as well"
+    );
+
+    return;
+}
+
+sub test_encoder {
+    my ( $input, $enkoder, $expected ) = @_;
+
+    my $output = enkode( $input, { max_length => 1, enkoder_index => $enkoder } );
+    is( $output, $expected, "enkoder $enkoder generates the expected output" );
+
+    return;
+}
+
+sub test_randomness {
+    my ( $input ) = @_;
+
+    my $output = enkode( $input );
+    cmp_ok( length $output, '>', 1024, "without limits the output is at least 1024 characters" );
+
+    for ( 1 .. 1000 ) {
+        my $output2 = enkode( $input );
+        return 1 if $output ne $output2;
+    }
+
+    return;
+}
+
+# note that for some reason triple backslashes (and possibly above) need to be
+# doubled in these definitions for tests to work
+sub enkoder_tests {
+    (
+        0 => q|
+<script type="text/javascript">
+/* <![CDATA[ */
+function perl_enkoder(){var kode=
+"kode=\";)\\\\\\"peüem\\\\\\"(etirw.tnemucod\";kode=kode.split('').reverse().join"+
+"('')"
+;var i,c,x;while(eval(kode));}perl_enkoder();
+/* ]]> */
+</script>
+|,
+        1 => q|
+<script type="text/javascript">
+/* <![CDATA[ */
+function perl_enkoder(){var kode=
+"kode=\"oducemtnw.iret\\\\\\"(emeü\\\\\\"p;)\";x='';for(i=0;i<(kode.length-1);i+="+
+"2){x+=kode.charAt(i+1)+kode.charAt(i)}kode=x+(i<kode.length?kode.charAt(ko"+
+"de.length-1):'');"
+;var i,c,x;while(eval(kode));}perl_enkoder();
+/* ]]> */
+</script>
+|,
+        2 => q|
+<script type="text/javascript">
+/* <![CDATA[ */
+function perl_enkoder(){var kode=
+"kode=\"grfxphqw1zulwh+%phÿhs%,>\";x='';for(i=0;i<kode.length;i++){c=kode.c"+
+"harCodeAt(i)-3;x+=String.fromCharCode(c)}kode=x"
+;var i,c,x;while(eval(kode));}perl_enkoder();
+/* ]]> */
+</script>
+|,
+    );
 }
